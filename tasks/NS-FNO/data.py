8cce7b68f2219d225c11cc95f665aed6b7c31f8e
Athreya Prakash <athreya@liftoffllc.com>
2025-07-03 16:25:19 +0200

    fixes

--- tasks/NS-FNO/data.py
+++ tasks/NS-FNO/data.py
@@ -30,7 +30,6 @@ def load_data(cfg: DictConfig):
     Reads data file name from task specific config, reads the data, pre-process it
     Returns the training and testing data loader of batch size
     '''
-    # Obtain path from config 
     file_path = os.path.join(cfg.data.root_dir, cfg.data.filename)
     
     dataset = read_data(file_path)
@@ -38,16 +37,16 @@ def load_data(cfg: DictConfig):
     # Rearrange to [N, T, C, H, W]
     dataset = dataset.permute(3, 0, 1, 2)   # [N, T, H, W]
     dataset = dataset.unsqueeze(2)         # Add channel dim → [N, T, 1, H, W]
+
+    # Wrap with custom Dataset BEFORE split
+    full_dataset = NavierStokesDataset(dataset)
+    data_tr, data_te = random_split(full_dataset, [cfg.data.n_tr, cfg.data.n_te])
     
-    data_tr, data_te = random_split(dataset, [cfg.data.n_tr, cfg.data.n_te])
-    
-    dataset_train = NavierStokesDataset(data_tr)
-    dataset_test = NavierStokesDataset(data_te)
-    
-    train_loader = DataLoader(dataset_train, batch_size=cfg.data.batch_size, shuffle=True)
-    test_loader = DataLoader(dataset_test, batch_size=cfg.data.batch_size, shuffle=False)
+    train_loader = DataLoader(data_tr, batch_size=cfg.data.batch_size, shuffle=True)
+    test_loader = DataLoader(data_te, batch_size=cfg.data.batch_size, shuffle=False)
 
     return train_loader, test_loader
+
 
 def load_testing_data(cfg: DictConfig):
     '''
