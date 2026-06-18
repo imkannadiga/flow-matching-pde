3f59cef37afe80023bcbe0d38628fbb7d3b5107d
athreyahprakash <st189828@stud.uni-stuttgart.de>
2026-06-18 14:28:00 +0200

    removed init

--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Model package; applies tensorly-torch / PyTorch empty-tensor compatibility before submodules load."""
-
-from models.tltorch_compat import apply_spectral_weight_empty_compat
-
-apply_spectral_weight_empty_compat()
